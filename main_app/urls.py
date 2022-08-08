a08190e24067f3c24cbc3b1226de81a7f430c52e
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-08 13:38:57 -0400

    creates basic detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path('', views.home, name=
   'home'),
   path('about/', views.about, name='about'),
-  path('gear/', views.gear_index, name='gear_index')
+  path('gear/', views.gear_index, name='gear_index'),
+  path('gear/<int:gear_id>/', views.gear_detail, name='gear_detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,7 @@ def about(request):
 def gear_index(request):
   gear = Gear.objects.all()
   return render(request, 'gear/index.html', {'gear': gear})
+
+def gear_detail(request, gear_id):
+  gear = Gear.objects.get(id=gear_id)
+  return render(request, 'gear/detail.html', { 'gear': gear})
