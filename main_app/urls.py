37c192613b0399a769cb56e9ade79d8791b30510
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-08 15:43:50 -0400

    adds sign up functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
   path('gear/create/', views.GearCreate.as_view(), name='gear_create'),
   path('gear/<int:pk>/update/', views.GearUpdate.as_view(), name='gear_update'),
   path('gear/<int:pk>/delete/', views.GearDelete.as_view(), name='gear_delete'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Gear
 
 class Home(LoginView):
@@ -10,7 +12,7 @@ def about(request):
   return render(request, 'about.html')
 
 def gear_index(request):
-  gear = Gear.objects.all()
+  gear = Gear.objects.filter(user=request.user)
   return render(request, 'gear/index.html', {'gear': gear})
 
 def gear_detail(request, gear_id):
@@ -32,3 +34,17 @@ class GearUpdate(UpdateView):
 class GearDelete(DeleteView):
   model = Gear
   success_url = '/gear/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('gear_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
