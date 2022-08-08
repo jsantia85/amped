7ba90f43c7809c461b9da87f9662a51bcd0fc6c3
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-08 11:26:39 -0400

    refactor gear index

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Gear
 
 def home(request):
   return render(request, 'home.html')
@@ -6,19 +7,6 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-class Gear:
-  def __init__(self, name, brand, description, price):
-    self.name = name
-    self.brand = brand
-    self.description = description
-    self.price = price
-
-gear = [
-  Gear('Telecaster', 'Fender', 'Great for math rock.', 699),
-  Gear('Stratocaster', 'Fender', 'Versitile, bright sounding.', 699),
-  Gear('Audio Interface', 'BV', 'Affordable', 150),
-  Gear('Chorus Pedal', 'Fender', 'Great modulator', 654)
-]
-
 def gear_index(request):
+  gear = Gear.objects.all()
   return render(request, 'gear/index.html', {'gear': gear})
